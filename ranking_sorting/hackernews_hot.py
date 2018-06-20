beafe6825d8f7e06cfedff19a4e9135a3f58ae9c
Raül Pérez <repejota@gmail.com>
2018-06-20 23:09:48 +0200

    Using timezones

--- ranking_sorting/hackernews_hot.py
+++ ranking_sorting/hackernews_hot.py
@@ -1,12 +1,13 @@
 import math
 import datetime
+from datetime import timezone
 
 
 def hackernews_hot(votes, published, gravity=1.8):
     """ Hackernews' hot sort
     """
 
-    d = datetime.datetime.now() - published
+    d = datetime.datetime.now(timezone.utc) - published
     hour_age = d.total_seconds() // 3600
     res = votes / math.pow(hour_age + 2, gravity)
     return res
--- tests/test_hackernews_hot.py
+++ tests/test_hackernews_hot.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime
+from datetime import timezone
 
 from ranking_sorting import hackernews_hot
 
@@ -8,7 +9,7 @@ class HackerNewsHotTest(unittest.TestCase):
 
     def test_hackernews_hot(self):
         expected = 0.0021404697161564834
-        published = datetime.datetime(year=2017, month=10, day=3, hour=17, minute=32, second=45)
+        published = datetime.datetime(year=2017, month=10, day=3, hour=17, minute=32, second=45).astimezone(timezone.utc)
         score = hackernews_hot.hackernews_hot(14520, published)
         self.assertAlmostEqual(expected, score, places=5)
 
